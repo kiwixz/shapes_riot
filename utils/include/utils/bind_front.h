1636cb32339db206acd78267708cce5bd3f80f3e
kiwixz <kiwixz@outlook.com>
2019-07-26 02:41:47 +0200

    use std bind_front when available

--- utils/include/utils/bind_front.h
+++ utils/include/utils/bind_front.h
@@ -6,18 +6,21 @@
 
 namespace utils {
 
-template <typename Function, typename... Args>
-[[nodiscard]] auto bind_front(Function&& callable, Args&&... args);
+template <typename F, typename... Args>
+[[nodiscard]] auto bind_front(F&& f, Args&&... args);
 
 
 // do not forget to wrap references in std::ref when appropriate
-template <typename Function, typename... Args>
-auto bind_front(Function&& callable, Args&&... args)
+template <typename F, typename... Args>
+auto bind_front(F&& f, Args&&... args)
 {
-    return [callable = std::forward<Function>(callable),
+#ifdef __cpp_lib_bind_front
+    return std::bind_front(std::forward<F>(f), std::forward<Args>(args)...);
+#else
+    return [callable = std::forward<F>(f),
             bound_args_tuple = std::make_tuple(std::forward<Args>(args)...)](auto&&... call_args) -> decltype(auto) {  // decltype to support references
         return std::apply([&](auto&&... bound_args)
-                          //noexcept(std::is_nothrow_invocable_v<Function, Args..., decltype(call_args)...>)  // propagate noexcept (not working?)
+                          //noexcept(std::is_nothrow_invocable_v<F, Args..., decltype(call_args)...>)  // propagate noexcept (not working?)
                           -> decltype(auto) {  // decltype to support references
                               return (std::invoke(callable,
                                                   std::forward<Args>(bound_args)...,
@@ -25,6 +28,7 @@ auto bind_front(Function&& callable, Args&&... args)
                           },
                           decltype(bound_args_tuple){bound_args_tuple});  // force a copy so we can call it multiple times
     };
+#endif
 }
 
 }  // namespace utils
