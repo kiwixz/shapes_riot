848eb46121ad7ae8b846e3185b394e58020f3d70
kiwixz <kiwixz@outlook.com>
2019-08-03 03:37:30 +0200

    add drawlist

---
+++ gfx/draw_list.cpp
@@ -0,0 +1,65 @@
+#include "gfx/draw_list.h"
+#include <glad/glad.h>
+
+namespace gfx {
+
+DrawList::DrawList()
+{
+    glCreateVertexArrays(vertex_array_.size, vertex_array_.ptr());
+    glCreateBuffers(buffers_.size, buffers_.ptr());
+
+    int pos_attrib = 0;
+    glEnableVertexArrayAttrib(vertex_array_[0], pos_attrib);
+    glVertexArrayVertexBuffer(vertex_array_[0], 0, buffers_[0], offsetof(ColoredVertex, pos), sizeof(ColoredVertex));
+    glVertexArrayAttribFormat(vertex_array_[0], pos_attrib, sizeof(ColoredVertex::pos) / sizeof(float), GL_FLOAT, GL_FALSE, 0);
+    glVertexArrayAttribBinding(vertex_array_[0], pos_attrib, 0);
+
+    int color_attrib = 1;
+    glEnableVertexArrayAttrib(vertex_array_[0], color_attrib);
+    glVertexArrayVertexBuffer(vertex_array_[0], 1, buffers_[0], offsetof(ColoredVertex, color), sizeof(ColoredVertex));
+    glVertexArrayAttribFormat(vertex_array_[0], color_attrib, sizeof(ColoredVertex::color) / sizeof(float), GL_FLOAT, GL_FALSE, 0);
+    glVertexArrayAttribBinding(vertex_array_[0], color_attrib, 1);
+
+    glVertexArrayElementBuffer(vertex_array_[0], buffers_[1]);
+
+    clear();
+}
+
+void DrawList::clear()
+{
+    vertices_.clear();
+    max_index_ = -1;
+}
+
+void DrawList::push(const std::vector<ColoredVertex>& vertices)
+{
+    vertices_.reserve(vertices_.size() + vertices.size());
+    std::copy(vertices.begin(), vertices.end(), std::back_inserter(vertices_));
+
+    indexes_.reserve(indexes_.size() + vertices.size());
+    for (int i = 0; i < vertices.size(); ++i)
+        indexes_.push_back(max_index_ + 1 + i);
+    max_index_ += static_cast<int>(vertices.size());
+}
+
+void DrawList::push(const std::vector<ColoredVertex>& vertices, const std::vector<Index>& index)
+{
+    vertices_.reserve(vertices_.size() + vertices.size());
+    std::copy(vertices.begin(), vertices.end(), std::back_inserter(vertices_));
+
+    indexes_.reserve(indexes_.size() + index.size());
+    for (int i = 0; i < index.size(); ++i)
+        indexes_.push_back(max_index_ + 1 + index[i]);
+    max_index_ += static_cast<int>(index.size());
+}
+
+void DrawList::draw()
+{
+    glBindVertexArray(vertex_array_[0]);
+    glNamedBufferData(buffers_[0], vertices_.size() * sizeof(ColoredVertex), vertices_.data(), GL_STREAM_DRAW);
+    glNamedBufferData(buffers_[1], indexes_.size() * sizeof(Index), indexes_.data(), GL_STREAM_DRAW);
+    glDrawElements(GL_TRIANGLES, static_cast<int>(indexes_.size()), GL_UNSIGNED_INT, nullptr);
+    glBindVertexArray(0);
+}
+
+}  // namespace gfx
---
+++ gfx/include/gfx/draw_list.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "gl_object.h"
+#include "utils/vec.h"
+#include <vector>
+
+namespace gfx {
+
+struct DrawList {
+    using Index = unsigned int;
+
+    struct ColoredVertex {
+        utils::Vec2f pos;
+        utils::Vec4f color;
+    };
+    static_assert(sizeof(ColoredVertex) == sizeof(float) * 6);
+
+    DrawList();
+
+    void clear();
+    void push(const std::vector<ColoredVertex>& vertices);
+    void push(const std::vector<ColoredVertex>& vertices, const std::vector<Index>& indexes);
+
+    void draw();
+
+private:
+    std::vector<ColoredVertex> vertices_;
+    std::vector<Index> indexes_;
+    int max_index_;
+
+    GlBuffers<2> buffers_;
+    GlVertexArrays<> vertex_array_;
+};
+
+}  // namespace gfx
