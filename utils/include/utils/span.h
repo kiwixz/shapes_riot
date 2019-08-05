e6e04c4dc41533a0270b556b2aeb008e74a3e70e
kiwixz <kiwixz@outlook.com>
2019-08-05 18:11:08 +0200

    utils span, add more test to utils

---
+++ utils/include/utils/span.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace utils {
+
+template <typename TElement>
+struct Span {
+    using Element = TElement;
+
+    constexpr Span() = default;
+
+    template <typename T>
+    constexpr Span(T& container);
+
+    constexpr Element& operator[](size_t idx) const;
+
+    constexpr Element* begin() const;
+    constexpr Element* data() const;
+    constexpr Element* end() const;
+    constexpr size_t size() const;
+
+
+private:
+    Element* data_;
+    size_t size_;
+};
+
+
+template <typename TElement>
+template <typename T>
+constexpr Span<TElement>::Span(T& container) :
+    data_{container.data()},
+    size_{container.size()}
+{}
+
+template <typename TElement>
+constexpr TElement& Span<TElement>::operator[](size_t idx) const
+{
+    return data_[idx];
+}
+
+template <typename TElement>
+constexpr TElement* Span<TElement>::begin() const
+{
+    return data_;
+}
+
+template <typename TElement>
+constexpr TElement* Span<TElement>::data() const
+{
+    return data_;
+}
+
+template <typename TElement>
+constexpr TElement* Span<TElement>::end() const
+{
+    return data_ + size_;
+}
+
+template <typename TElement>
+constexpr size_t Span<TElement>::size() const
+{
+    return size_;
+}
+
+}  // namespace utils
---
+++ utils/span.test.cpp
@@ -0,0 +1,43 @@
+#include "utils/span.h"
+#include <doctest/doctest.h>
+#include <array>
+#include <vector>
+
+namespace utils::test {
+
+TEST_SUITE("span")
+{
+    TEST_CASE("simple")
+    {
+        std::vector<int> container = {1, 2, 3};
+        Span<int> span = container;
+        CHECK(*span.begin() == 1);
+        CHECK(span.data() == container.data());
+        CHECK(span[2] == 3);
+        CHECK(span.size() == container.size());
+        span[1] = 4;
+        CHECK(container[1] == 4);
+    }
+
+    TEST_CASE("const")
+    {
+        std::vector<int> container = {1, 2, 3};
+        Span<const int> span = container;
+        CHECK(*span.begin() == 1);
+        CHECK(span.data() == container.data());
+        CHECK(span[2] == 3);
+        CHECK(span.size() == container.size());
+    }
+
+    TEST_CASE("constexpr")
+    {
+        constexpr std::array<const char, 6> container{"hello"};
+        constexpr Span<const char> span = container;
+        static_assert(*span.begin() == 'h');
+        static_assert(span.data() == container.data());
+        static_assert(span[2] == 'l');
+        static_assert(span.size() == container.size());
+    }
+}
+
+}  // namespace utils::test
