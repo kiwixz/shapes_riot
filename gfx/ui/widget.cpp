b199c87aeb890957ba17264fef557be6522ba61d
kiwixz <kiwixz@outlook.com>
2019-08-09 02:43:34 +0200

    widget on_window_event

--- gfx/ui/widget.cpp
+++ gfx/ui/widget.cpp
@@ -3,6 +3,11 @@
 namespace gfx::ui {
 
 void Widget::on_window_event(const WindowEvent& event, const WindowState& state)
-{}
+{
+    if (const auto* key_event = event.as<gfx::WindowEvent::KeyEvent>())
+        on_key(*key_event);
+    else if (const auto* mouse_button_event = event.as<gfx::WindowEvent::MouseButtonEvent>())
+        on_mouse_button(*mouse_button_event, state.mouse_pos);
+}
 
 }  // namespace gfx::ui
