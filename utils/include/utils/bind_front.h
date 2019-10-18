69f18737061a0816d9e28d8245b193613d11962e
kiwixz <kiwixz@outlook.com>
2019-10-18 20:44:36 +0200

    cleanup bind_front

--- utils/include/utils/bind_front.h
+++ utils/include/utils/bind_front.h
@@ -21,14 +21,12 @@ template <typename F, typename... Args>
 constexpr auto bind_front(F&& f, Args&&... args)
 {
     return [callable = std::forward<F>(f),
-            bound_args_tuple = std::make_tuple(std::forward<Args>(args)...)](auto&&... call_args) -> decltype(auto) {  // decltype to support references
-        return std::apply([&](auto&&... bound_args)
-                          //noexcept(std::is_nothrow_invocable_v<F, Args..., decltype(call_args)...>)  // propagate noexcept (not working?)
-                          -> decltype(auto) {  // decltype to support references
-                              return (std::invoke(callable,
-                                                  std::forward<Args>(bound_args)...,
-                                                  std::forward<decltype(call_args)>(call_args)...));
-                          },
+            bound_args_tuple = std::make_tuple(std::forward<Args>(args)...)](auto&&... call_args) -> decltype(auto) {
+        return std::apply([&](auto&&... bound_args) -> decltype(auto) {
+            return std::invoke(callable,
+                               std::forward<Args>(bound_args)...,
+                               std::forward<decltype(call_args)>(call_args)...);
+        },
                           decltype(bound_args_tuple){bound_args_tuple});  // force a copy so we can call it multiple times
     };
 }
