74698f1e5bc2ba086f0cc3e8a1d9352394f43a36
kiwixz <kiwixz@outlook.com>
2019-12-30 21:14:46 +0100

    add anchor enum

---
+++ gfx/include/gfx/anchor.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace gfx {
+
+enum class Anchor {
+    center,
+    top_left,
+    top,
+    top_right,
+    right,
+    bottom_right,
+    bottom,
+    bottom_left,
+    left,
+};
+
+}  // namespace gfx
