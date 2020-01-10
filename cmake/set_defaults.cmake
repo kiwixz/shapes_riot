1dec8e4c615342a822ae0ca3e4aad9c1c837d119
kiwixz <kiwixz@outlook.com>
2020-01-10 20:32:50 +0100

    everything imply pedantic

--- cmake/set_defaults.cmake
+++ cmake/set_defaults.cmake
@@ -26,7 +26,7 @@ macro (set_defaults_flags_cpp)
 
     if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
         set(CMAKE_CXX_FLAGS
-            "${CMAKE_CXX_FLAGS} -pedantic -Weverything  \
+            "${CMAKE_CXX_FLAGS} -Weverything  \
             -Wno-c++98-compat-pedantic -Wno-ctad-maybe-unsupported -Wno-double-promotion -Wno-exit-time-destructors  \
             -Wno-float-equal -Wno-padded -Wno-sign-conversion -Wno-weak-vtables  \
             \
